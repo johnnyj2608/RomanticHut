6d71daca3000c03817bac151a20ea1d4f8b8101b
johnnyj2608 <54607786+johnnyj2608@users.noreply.github.com>
2023-06-02 11:47:19 -0400

    Email if limit > 10

--- Twilio.js
+++ Twilio.js
@@ -22,7 +22,7 @@ function sendSms(to, body) {
 function avail(e) {
 
   // Ensure it's the correct form being requested
-  if (e.source.getActiveSheet().getName() != 'Avail' || limiter() > 10) {
+  if (e.source.getActiveSheet().getName() != 'Avail') {
     return;
   }   
   var timestamp = 1;
@@ -32,29 +32,34 @@ function avail(e) {
   var number = 5;
   var message = 'Romantic Hut Party Hall';
 
-// Variables to check if proposed date is possible
+  // Variables to check if proposed date is possible
   var ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Avail");
   var last = ss.getRange("A1:A").getValues().filter(String).length;
   var today = Utilities.formatDate(new Date(), "GMT+19", 'MMMM d, yyyy');
   var date = ss.getRange(last, date).getValue();
   var newDate = Utilities.formatDate(date, "GMT+19", 'MMMM d, yyyy');
   var number = ss.getRange(last, number).getValue();
+
+  if (limiter()+20 > 10) {
+    email(number);
+    return;
+  }
   
-// Create variables of proposed details
+  // Create variables of proposed details
   if (new Date(newDate) >= new Date(today)) {
     var name = ss.getRange(last, name).getValue();
     var room = ss.getRange(last, room).getValue();
     var day = Utilities.formatDate(date, "GMT+19", 'EEEE');
     var status = '';
 
-// Create list of reserved dates
+  // Create list of reserved dates
     var public = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Public");
     var startRow = 3;
     var numRows = public.getRange("A1:A").getValues().filter(String).length;
     var dataRange = public.getRange(startRow, 1, numRows, 2);
     var data = dataRange.getValues();
 
-// If date and room pair matches, FULL. If proposed date passed, OPEN
+  // If date and room pair matches, FULL. If proposed date passed, OPEN
     for (i in data) {
       var row = data[i];
       var curDate = Utilities.formatDate(row[0], "GMT+19", 'MMMM d, yyyy');
@@ -85,7 +90,7 @@ function avail(e) {
 function visit(e) {
 
   // Ensure it's the correct form being requested
-  if (e.source.getActiveSheet().getName() != 'Visit' || limiter() > 10) {
+  if (e.source.getActiveSheet().getName() != 'Visit') {
     return
   }
   var timestamp = 1;
@@ -107,6 +112,11 @@ function visit(e) {
   var time = ss.getRange(last, time).getValue();
   var cur_time = (new Date(time).getHours())*60+(new Date(time).getMinutes());
   var number = ss.getRange(last, number).getValue();
+
+  if (limiter()> 10) {
+    email(number);
+    return;
+  }
 
   // Create variables of proposed details
   if (new Date(newDate) >= new Date(today) && cur_time >= startTime && cur_time <= endTime && new Date(newDate).getDay() > sunday) {
@@ -154,6 +164,7 @@ function limiter() {  // Same date from timestamp and phone number. Email if if 
   var visitRange = visit.getRange(startRow, 1, numRows, 5);
   var visitData = visitRange.getValues();
 
+  // Count date and number pairs of visit form
   for (i in visitData) {
     var row = visitData[i];
     var curDate = Utilities.formatDate(new Date(row[0]), "GMT+19", 'MMMM d, yyyy');
@@ -163,7 +174,7 @@ function limiter() {  // Same date from timestamp and phone number. Email if if 
     }
   }
 
-  number = 5;
+  number = 5;   // Reset var to column #
   var avail = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Avail");
   var lastAvail = avail.getRange("A1:A").getValues().filter(String).length;
   var number = avail.getRange(lastAvail, number).getValue();
@@ -172,6 +183,7 @@ function limiter() {  // Same date from timestamp and phone number. Email if if 
   var availRange = avail.getRange(startRow, 1, numRows, 5); 
   var availData = availRange.getValues();
 
+  // Count date and number pairs of avail form
   for (i in availData) {
     var row = availData[i];
     var curDate = Utilities.formatDate(new Date(row[0]), "GMT+19", 'MMMM d, yyyy');
@@ -181,4 +193,8 @@ function limiter() {  // Same date from timestamp and phone number. Email if if 
     } 
   }
   return count;
+}
+
+function email(message) {
+  MailApp.sendEmail('email@gmail.com', 'Romantic Hut Party Hall', message+' has attempted 10 requests');
 }
