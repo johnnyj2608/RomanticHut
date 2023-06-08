3cbcd7162416cf43bbc49a7a85c146b4a16a23e5
Johnny Jiang <johnnyjiang@johnnys-mbp.lan>
2023-06-07 22:54:30 -0400

    Final content arrangement. More animations. Gallery in container

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const menu = document.querySelector('#mobile-menu');
+/*const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
 menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+*/
+
+function jump(h){
+  var top = document.getElementById(h).offsetTop;
+  window.scrollTo(0, top);
+}
