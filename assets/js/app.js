d604431926af3d960400a2ab6d431614de10c946
Johnny Jiang <johnnyjiang@Johnnys-MBP.domain>
2023-06-09 18:39:43 -0400

    Moved scripts to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,37 @@
+function start() {
+  AOS.init();
+  new Splide( '.splide' ).mount();
+
+document.addEventListener('DOMContentLoaded', function () {
+  var main = new Splide('#main-slider', {
+      type: 'fade',
+      rewind: true,
+      pagination: false,
+      arrows: false,
+  });
+
+  var thumbnails = new Splide('#thumbnail-slider', {
+      fixedWidth: 160,
+      fixedHeight: 80,
+      gap: 10,
+      rewind: true,
+      pagination: false,
+      cover: true,
+      isNavigation: true,
+      breakpoints: {
+          600: {
+              fixedWidth: 60,
+              fixedHeight: 44,
+          },
+      },
+  });
+
+  main.sync(thumbnails);
+  main.mount();
+  thumbnails.mount();
+});
+}
+
 function jump(h){
   var top = document.getElementById(h).offsetTop;
   window.scrollTo(0, top);
